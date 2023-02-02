13f11450a6989a1934cbfd629c0761e655d5fea8
Sidney Carlini Junior <sidney_carlini@hotmail.com>
2023-02-01 23:40:17 -0300

    home and Next Images

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,19 @@
 import type { AppProps } from 'next/app'
 
+import { globalStyles } from '../styles/global'
+import { Container, Header } from '@/styles/pages/app'
+
+import logo from '../assets/logo.svg'
+import Image from 'next/image'
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  globalStyles();
+  return (
+    <Container>
+      <Header>
+        <Image src={logo} alt="logo" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
 }
