2a43b5cbd8e14ae5a7e4d3f0e081ceb7a0ca3bec
Sidney Carlini Junior <sidney_carlini@hotmail.com>
2023-02-10 21:20:12 -0300

    Next link é insano

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { GetStaticProps } from "next";
 import Image from "next/image";
+import Link from "next/link";
 
 import { useKeenSlider } from 'keen-slider/react'
 
@@ -29,13 +30,15 @@ export default function Home({ products }: HomeProps) {
     <HomeContainer ref={sliderRef} className="keen-slider">
       {products.map((product) => {
         return (
-          <Product key={product.id} className="keen-slider__slide">
-            <Image src={product.imageUrl} width={520} height={480} alt={"t-shirt"} />
-              <footer>
-                <strong>{product.name}</strong>
-                <p>{product.price}</p>
-              </footer>
-          </Product>
+          <Link href={`/product/${product.id}`} key={product.id}>
+            <Product className="keen-slider__slide">
+              <Image src={product.imageUrl} width={520} height={480} alt={"t-shirt"} />
+                <footer>
+                  <strong>{product.name}</strong>
+                  <p>{product.price}</p>
+                </footer>
+            </Product>
+          </Link>
           )
       })}
     </HomeContainer>
