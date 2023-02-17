ba535fca3e7a2b712fc5a7d6b1b0c57e65de8028
Sidney Carlini Junior <sidney_carlini@hotmail.com>
2023-02-16 23:36:36 -0300

    styling

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,7 +1,20 @@
+import { ImageContainer, ProductContainer, ProductDetails } from "@/styles/pages/product";
 import { useRouter } from "next/router"
 export default function Product(){
     const { query } = useRouter();
     return(
-        <h1>Product: {JSON.stringify(query)}</h1>
+        <ProductContainer>
+            <ImageContainer>
+                 
+            </ImageContainer>
+            <ProductDetails>
+                <h1>Camiseta X</h1>
+                <span>R$ 79,90</span>
+
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate molestiae error excepturi eveniet aperiam veritatis quos voluptatum repellendus quasi dolores magnam laboriosam rem, est repellat, dolore officia quia perspiciatis maiores.</p>
+
+                <button>Comprar agora</button>
+            </ProductDetails>
+        </ProductContainer>
     )
 }
